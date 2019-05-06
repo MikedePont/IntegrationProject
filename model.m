51eb68304b1b1e58c40dd7771bad16d193f822a9
MikedePont <m.depont@hotmail.com>
2019-05-06 17:00:46 +0200

    Files geordend

--- model.m
+++ model.m
@@ -1,3 +1,7 @@
+clear all
+close all
+clc
+%% Parameters
 mc = 0.5;
 mp = 0.2;
 b = 0.1;
@@ -5,41 +9,7 @@ d = 1;
 I = 0.006;
 g = 9.8;
 L = 0.3;
-%%
-
-q = (M+m)*(I+m*l^2)-(m*l)^2;
-s = tf('s');
-
-P_cart = (((I+m*l^2)/q)*s^2 - (m*g*l/q))/(s^4 + (b*(I + m*l^2))*s^3/q - ((M + m)*m*g*l)*s^2/q - b*m*g*l*s/q);
-
-P_pend = (m*l*s/q)/(s^3 + (b*(I + m*l^2))*s^2/q - ((M + m)*m*g*l)*s/q - b*m*g*l/q);
-
-sys_tf = [P_cart ; P_pend];
-
-inputs = {'u'};
-outputs = {'x'; 'phi'};
-
-set(sys_tf,'InputName',inputs)
-set(sys_tf,'OutputName',outputs)
-
-sys_tf = c2d(sys_tf,0.1);
-
-%% Second system
-h=0.1;
-M = 0.5;
-m = 0.2;
-b = 0.1;
-d = 1;
-I = 0.006;
-g = 9.8;
-L = 0.3;
-
-A = [0,1,0,0;0,-d/M, -m*g/M,0;0,0,0,1;0,-d/(M*L), (m+M)*g/(M*L),0];
-B = [0;1/M;0;1/(M+L)];
-C = [1,0,0,0;0,0,1,0];
-D = 0;
-
-newsys = c2d(ss(A,B,C,D),h);
+h = 0.01;
 
 %%
 sys = pend_dynamics(mc,mp,g,L,h);
@@ -63,6 +33,3 @@ for k = 1:length(Y)
     end
     drawnow 
 end
-
-%% Via nonlinear dynamical model
-[t,y] = ode45(@(t,y)pend_dynamics(y,mc,mp,g,L,-K*(y-[1; 0; pi; 0])),tspan,x0);
